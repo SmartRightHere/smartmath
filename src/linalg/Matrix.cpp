c2259441f3d4cba6b34b4e3eeaf059dfda5aa8d6
Alexandr <thebarsik26@gmail.com>
2025-11-22 22:47:36 +0300

    matrix test class is coded

--- src/linalg/Matrix.cpp
+++ src/linalg/Matrix.cpp
@@ -1,6 +1,23 @@
 #include <iostream>  
-using namespace std;  
-int main() {  
-    cout << "Hello World!" << endl;  
-    return 0;  
+#include <Windows.h>
+using namespace std;
+
+class Matrix {
+public:
+    unsigned int rows;
+    unsigned int columns;
+    void print() {
+        cout << "rows: " << rows <<
+        "\tcolumns: " << columns << endl;
+    }
+};
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Matrix matrix;
+    matrix.rows = 3;
+    matrix.columns = 3;
+    matrix.print();
 }
